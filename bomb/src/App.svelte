a37597949190dfcee3f575b87e0861e3335122e9
emma-gran-nti-johanneberg <74712733+emma-gran-nti-johanneberg@users.noreply.github.com>
2022-05-09 15:44:07 +0200

    Hämtat foton och börjat prog.

--- bomb/src/App.svelte
+++ bomb/src/App.svelte
@@ -1,25 +1,59 @@
 <script>
-    export let name;
-  </script>
-  
-  <style lang="scss">
-    $color: red;
-  
-    h1 {
-      color: $color;
+    function generateDeck(){
+
+        let deck = [];
+
+        for(i=1; i<10; i++){
+
+            let card = {
+                id: i,
+                img: `char-${i}.png`,
+                bomb: false,
+                hidden: true,
+            }
+            //Hämtar korten 
+            deck.push(card) 
+        }
+        return deck;
     }
-  
-    div {
-      background: green;
-  
-      > p {
-        color: #fff;
-      }
-    }
-  </style>
-  
-  <h1>Hello {name}!</h1>
-  
-  <div>
-    <p>SASS is working!</p>
-  </div>
+
+    
+function updateUI(){
+
+document.querySelector('main').innerHTML = '';
+
+    // loopa igenom våra kort
+    deck.forEach(card => {
+    
+        let neutral = "";
+        if(card.neutral){
+            neutral = "neutral";
+        }
+
+        let el = `<article data-id="${card.id}" class="${neutral}">
+            <img src="img/${card.img}" alt="card">
+            </article>`;
+
+        document
+        .querySelector('main')
+        .insertAdjacentHTML('beforeend', el);
+
+    })
+// generera HTML för varje kort
+
+// Lyssna på klick på varje kort
+
+}
+</script>
+
+<main>
+    <div class="time">Tiden ska stå här</div>
+    <div class="score">Här kommer poängen att stå</div>
+    <div class="wrapper">
+
+    </div>
+</main>
+
+<style lang="scss">
+	
+</style>
