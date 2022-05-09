67751dcf55be4c55342b7b30fa87c39bc4444362
emma-gran-nti-johanneberg <74712733+emma-gran-nti-johanneberg@users.noreply.github.com>
2022-05-09 15:15:11 +0200

    SASS-test
    Detta är för att testa SASS:en då den var markerad röd i app.svelte men med detta testet verkar det som om scss fungerar och mer tid behöver inte lägas på att lösa problemet

--- bomb/src/App.svelte
+++ bomb/src/App.svelte
@@ -1,10 +1,25 @@
 <script>
-</script>
-
-<main>
-
-</main>
-
-<style lang="scss">
-	
-</style>
+    export let name;
+  </script>
+  
+  <style lang="scss">
+    $color: red;
+  
+    h1 {
+      color: $color;
+    }
+  
+    div {
+      background: green;
+  
+      > p {
+        color: #fff;
+      }
+    }
+  </style>
+  
+  <h1>Hello {name}!</h1>
+  
+  <div>
+    <p>SASS is working!</p>
+  </div>
