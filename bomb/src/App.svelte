b57f1f041511c9e6d4bc51ef6d1473dada96baf3
emma-gran-nti-johanneberg <74712733+emma-gran-nti-johanneberg@users.noreply.github.com>
2022-05-09 16:50:01 +0200

    SCSS och js fungerar

--- bomb/src/App.svelte
+++ bomb/src/App.svelte
@@ -2,8 +2,8 @@
     function generateDeck(){
 
         let deck = [];
-
-        for(i=1; i<10; i++){
+        
+        for(let i=1; i<10; i++){
 
             let card = {
                 id: i,
@@ -17,43 +17,108 @@
         return deck;
     }
 
+    let deck = generateDeck()
+
+    /*
+    //function updateUI(){
+
+        document.querySelector('.wrapper').innerHTML = '';
+
+        // loopa igenom våra kort
+        deck.forEach(card => {
     
-function updateUI(){
+            let hidden = "";
+            if(card.hidden){
+                hidden = "hidden";
+            }
 
-document.querySelector('main').innerHTML = '';
-
-    // loopa igenom våra kort
-    deck.forEach(card => {
-    
-        let neutral = "";
-        if(card.neutral){
-            neutral = "neutral";
-        }
-
-        let el = `<article data-id="${card.id}" class="${neutral}">
-            <img src="img/${card.img}" alt="card">
+            let el = `<article data-id="${card.id}" class="${hidden}">
+                <img src="img/${card.img}" alt="card">
             </article>`;
 
-        document
-        .querySelector('main')
-        .insertAdjacentHTML('beforeend', el);
+            document
+            .querySelector('.wrapper')
+            .insertAdjacentHTML('beforeend', el);
 
-    })
-// generera HTML för varje kort
+        })
+    // generera HTML för varje kort
+    // Lyssna på klick på varje kort
 
-// Lyssna på klick på varje kort
+    }
 
-}
+
+    // Detta ska generera och updatera decket med kort
+    let deck = generateDeck();
+    console.log(deck);
+    updateUI();
+
+    let card1 = null;
+
+    function flippCard(card){
+        card.classList.remove("hidden");
+    }
+
+    for(i=0; i<card.length; i++){
+        card[i].addEventListener('click', (e) => {
+            card1 = e.target.parentNode.getAttribute(`data-id`)
+            flippCard(e.target.parentNode)
+        }
+    }
+
+*/
+
+
+
 </script>
 
 <main>
-    <div class="time">Tiden ska stå här</div>
-    <div class="score">Här kommer poängen att stå</div>
+    <nav>
+        <div class="time">Tiden ska stå här</div>
+        <div class="score">Här kommer poängen att stå</div> 
+    </nav>
     <div class="wrapper">
+        {#each deck as card}
+        <article class="card">
+        <img src="img/{card.img}">
+        </article>
+        {/each}
 
     </div>
 </main>
 
 <style lang="scss">
-	
+    main{
+            height: 75vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        nav {
+            margin: 2rem;
+        }
+
+        .wrapper{
+            background-color: gray;
+            padding: 3rem;
+            display: grid;
+            grid-template-columns: repeat(3, 150px);
+            gap: 1rem;
+            align-items: center;
+
+            img{
+                width: 100%;
+                opacity: 1;
+                background-color: green;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: column;
+
+                .hidden{
+                    opacity: 0;
+                }
+            }
+            
+        }
+    }
 </style>
